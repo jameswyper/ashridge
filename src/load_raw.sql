4230c70fc1d6a74a8655dd056aaa09fc54ce5ae3
James Wyper <jrwyper@yahoo.co.uk>
2024-08-09 20:49:52 +0000

    change to saved reports

--- src/load_raw.sql
+++ src/load_raw.sql
@@ -14,10 +14,10 @@ drop table if exists raw_players_poa_not_verified;
 .import managers.csv raw_managers
 .import teams_all.csv raw_teams
 .import players_all.csv raw_players_all
-.import players__East_Berks_Football_Alliance__ByTeam-FAN_Pending-Denied.csv raw_players_fan_pending_denied
-.import players__East_Berks_Football_Alliance__EBFA-No_24-25_GS-LPGAF.csv raw_players_no_lpgaf
-.import players__East_Berks_Football_Alliance__PL01.1_Photo_NOT_Loaded.csv raw_players_no_photo
-.import players__East_Berks_Football_Alliance__ByTeam-No_POA_Uploaded.csv raw_players_no_poa
-.import players__East_Berks_Football_Alliance__PL02.1_POA_NOT_Vertified.csv raw_players_poa_not_verified
+.import players__East_Berks_Football_Alliance__PL01.1_ByTeam-FAN_Pending-Denied_-_CL.csv raw_players_fan_pending_denied
+.import players__East_Berks_Football_Alliance__PL01.5_ByTeam-No_24-25_GS-LPGAF_-_CL.csv raw_players_no_lpgaf
+.import players__East_Berks_Football_Alliance__PL02.2_Photo_NOT_Loaded_-_CL.csv raw_players_no_photo
+.import players__East_Berks_Football_Alliance__PL01.3_ByTeam-No_POA_Uploaded_-_CL.csv  raw_players_no_poa
+.import players__East_Berks_Football_Alliance__PL02.4_EBFA-POA_NOT_Vertified_-_CL.csv raw_players_poa_not_verified
 
 /* note that the tables will actually named raw.coaches and so on */
