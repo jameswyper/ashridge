122d6be1671dce46fdf3e27edf68fb9e23953e2d
James Wyper <jrwyper@yahoo.co.uk>
2024-12-07 18:16:58 +0000

    Fixed FA Manager dowload to go to new website

--- src/fa_manager_dl.rb
+++ src/fa_manager_dl.rb
@@ -1,106 +1,77 @@
-#!/usr/bin/env ruby
+
 require 'selenium-webdriver'
 require 'netrc'
 $stdout.sync = true
 user, pass = Netrc.read["wholegame.thefa.com"]
-puts "Starting.."
-
-begin
-
-options = Selenium::WebDriver::Firefox::Options.new
-d = Selenium::WebDriver.for :remote, url: 'http://localhost:4444', options: options
-
-wait = Selenium::WebDriver::Wait.new(:timeout => 30)
-d.manage.timeouts.implicit_wait = 30
-d.manage.delete_all_cookies
-d.get('https://wholegame.thefa.com')
-
-puts "Loaded first page, waiting for cookie pop-up"
-
-wait.until { d.find_element(:id ,"onetrust-accept-btn-handler") }
-cookie = d.find_element(:id,"onetrust-accept-btn-handler")
-sleep 1
-if (cookie) then cookie.click end
-sleep 1
-puts "Waiting to sign in"
-wait.until {d.find_element(:id,"signInName")}
-d.find_element(:id,"signInName").send_keys user
-d.find_element(:id,"password").send_keys pass
-wait.until { d.find_element(:id , "next") }
-button = d.find_element(:class,"buttons")
-signin = d.find_element(:id,"next")
-d.action.move_to(button).perform
-signin.click
-puts "Signing in.."
-sleep 5
-puts "Waiting for another cookie pop-up.."
-wait.until { d.find_element(:id ,"BtnAcceptCookies") }
-c = d.find_element(:id ,"BtnAcceptCookies")
-wait.until {c.displayed?}
-#puts "button text is "+ d.find_element(:id , "BtnAcceptCookies").text
-d.find_element(:id,"BtnAcceptCookies").click   
-
-puts "Going to Club Secretary tab"
-
-wait.until { d.find_element(:partial_link_text,"Club Secretary") }
-club = d.find_element(:partial_link_text,"Club Secretary")   
-club.click
-
-puts "Going to Team Officials tab"
-
-wait.until { d.find_element(:link_text,"Team Officials") }
-team = d.find_element(:link_text,"Team Officials" )
-team.click
-
-sleep 10
-
-puts "Accepting cookies again"
-wait.until { d.find_element(:id ,"onetrust-accept-btn-handler") }
-cookie = d.find_element(:id,"onetrust-accept-btn-handler")
-sleep 1
-if (cookie) then cookie.click end
-sleep 1
-
-puts "Clicking on Safeguarding Tab"
-
-safexpath  = "/html/body/app-root/div[1]/app-portal/div/div[2]/app-core/div/mat-sidenav-container/mat-sidenav-content/official-listing/div/div[1]/div[3]/app-secondary-navigation/div/div/div[3]"
-
-wait.until { d.find_element(:xpath,safexpath) }
-safe = d.find_element(:xpath,safexpath)
-raise "Unexpected text on Safeguarding tab: #{safe.text}" unless safe.text == "Safeguarding & Qualifications"
-safe.click
-sleep 5
-puts "Clicking on Export"
-exportxpath = "/html/body/app-root/div[1]/app-portal/div/div[2]/app-core/div/mat-sidenav-container/mat-sidenav-content/official-listing/div/div[2]/safeguarding-listing/div/div[1]/div[1]/div[1]/div/div[2]/button"
 
 
-wait.until {d.find_element(:xpath,exportxpath)}
-export = d.find_element(:xpath,exportxpath)
-raise "Unexpected text on Export button: #{export.text}" unless export.text == "Export Team Officials"
-d.action.move_to(export).perform
-export.click
-sleep 1
+class Driver
+    def initialize(site)
+        @options = Selenium::WebDriver::Firefox::Options.new
+        @d = Selenium::WebDriver.for :remote, url: 'http://localhost:4444', options: @options
 
-
-puts "Clicking on Download"
-#dlxpath = "/html/body/div[2]/div[2]/div/mat-dialog-container/export-team-officials-popup/div/div[2]/div[2]/button[2]"
-dlcss = "button.dialog-btn:nth-child(2)"
-
-#puts d.find_element(:xpath,"/html/body").attribute(:innerHTML)
-wait.until {d.find_element(:css,dlcss)}
-dl = d.find_element(:css,dlcss)
-
-
-#wait.until {d.find_element(:xpath,dlxpath)}
-#dl = d.find_element(:xpath,dlxpath)
-raise "Unexpected text on Download button: #{dl.text}" unless dl.text.downcase == "download"
-dl.click
-sleep 15
-
-ensure
-
-d.quit
-
+        @wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+        @d.manage.timeouts.implicit_wait = 30
+        @d.manage.delete_all_cookies
+        @d.get(site)
+    end
+    def click(css,msg=nil)
+        puts msg if msg
+        @wait.until {@d.find_element(:css,css)}
+        thing = @d.find_element(:css,css)
+        @d.action.move_to(thing,0,-5).perform
+        thing.click
+        return thing
+    end
+    def send(css,input)
+        @wait.until {@d.find_element(:css,css)}
+        thing = @d.find_element(:css,css)
+        thing.send_keys input
+    end
+    def find(css)
+        @wait.until {@d.find_element(:css,css)}
+        return @d.find_element(:css,css)
+    end
+    def quit 
+        @d.quit
+    end
 end
 
 
+puts "Starting.. User is #{user}"
+begin
+
+    dl = Driver.new('https://clubs.thefa.com')
+    puts "Loaded first page, waiting for cookie pop-up"
+    begin
+        dl.click('#onetrust-accept-btn-handler')
+    rescue Selenium::WebDriver::Error
+    end
+
+    sleep 1
+
+
+    puts "Waiting to sign in"
+
+    dl.send('#signInName',user)
+    dl.send('#password',pass)
+    sleep 0.1
+    dl.click('html body div#panel.panel table.panel_layout tbody tr.panel_layout_row td#panel_center div.inner_container div.api_container.normaltext div#api form#localAccountForm.localAccount div.entry div.buttons button#next')
+    puts "Signing in.."
+    sleep 10
+
+    dl.click('#primary-navigation > div.main-ctr > div.navigation-menu > div.module-ctr > div:nth-child(5)',"Going to Officials")
+    dl.click('#secondary-navigation > div > div:nth-child(3)',"Going to Safeguarding")
+    sleep 5
+    dl.click('#safeguarding-listing > div.fixed-content > div.toolbar-ctr > div.top-bar > div > div:nth-child(2) > button',"Clicking Export")
+    sleep 2
+    dl.click('#export-officials > div.popup-body > div.dialog-actions > button.dialog-btn.emphasised-btn.fa-blue',"Clicking Download")
+
+
+
+
+ensure
+
+dl.quit
+
+end
