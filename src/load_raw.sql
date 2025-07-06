a3adb72ad1a29deabdfe350e208f339e93056d62
James Wyper <jrwyper@yahoo.co.uk>
2025-07-06 07:48:23 +0000

    fix some report name changes

--- src/load_raw.sql
+++ src/load_raw.sql
@@ -15,9 +15,9 @@ drop table if exists raw_players_poa_not_verified;
 .import teams_all.csv raw_teams
 .import players_all.csv raw_players_all
 .import players__East_Berks_Football_Alliance__PL02.1_Player_FAN_NOT_Approved_-_CL.csv raw_players_fan_pending_denied
-.import players__East_Berks_Football_Alliance__PL02.5_EBFA-No_24-25_GS-LPGAF_-_CL.csv raw_players_no_lpgaf
+.import players__East_Berks_Football_Alliance__PL02.5_No_GS-LPGAF_-_CL.csv raw_players_no_lpgaf
 .import players__East_Berks_Football_Alliance__PL02.2_Player_Photos_NOT_Loaded_-_CL.csv raw_players_no_photo
-.import players__East_Berks_Football_Alliance__PL01.3_ByTeam-POA-POBP_NOT_Uploaded_-_CL.csv  raw_players_no_poa
-.import players__East_Berks_Football_Alliance__PL02.4_EBFA-POA_NOT_Vertified_-_CL.csv raw_players_poa_not_verified
+.import players__East_Berks_Football_Alliance__PL01.3_ByTeam-POA-POBP_NOT_Approved_-_CL.csv  raw_players_no_poa
+.import players__East_Berks_Football_Alliance__PL02.4_POA_NOT_Vertified_-_CL.csv raw_players_poa_not_verified
 
-/* note that the tables will actually named raw.coaches and so on */
+/* note that the tables will actually be named raw.coaches and so on */
